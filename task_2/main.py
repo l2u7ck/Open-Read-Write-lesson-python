54fb4d0f0ab4007d56ac783ba42f8656a2008194
anton <l2u7ck@gmail.com>
2024-05-01 17:02:47 +0300

    ChangeFileTwo

--- task_2/main.py
+++ task_2/main.py
@@ -1,17 +1,34 @@
 # The function takes as input a file with dishes and forms a dictionary of dishes on its basis
-def food_dict(recipes):
-    book = dict()
+def food_recipes(recipes):
+    book_recipes = dict()
     for dish in recipes:
         dish = dish.strip()
-        book[dish] = []
+        book_recipes[dish] = []
         count = int(recipes.readline())
         for x in range(count):
             line = recipes.readline().strip().split(" | ")
             ingredient = {'ingredient_name': line[0], 'quantity': line[1], 'measure': line[2]}
-            book[dish].append(ingredient)
+            book_recipes[dish].append(ingredient)
         recipes.readline()
-    return book
+    return book_recipes
 
 
-cook_book = food_dict(open('recipes.txt', encoding='utf-8'))
+cook_book = food_recipes(open('recipes.txt', encoding='utf-8'))
 print(cook_book)
+
+
+# The function takes as input a list of dishes and the number of people, and outputs the number of ingredients
+def get_shop_list_by_dishes(dishes, person_count):
+    ingredients_count = dict()
+    for dish in dishes:
+        for ingredient in cook_book[dish]:
+            if ingredient['ingredient_name'] in ingredients_count:
+                ingredients_count[ingredient['ingredient_name']]['quantity'] += int(ingredient['quantity'])*person_count
+            else:
+                ingredients_count[ingredient['ingredient_name']] = {}
+                ingredients_count[ingredient['ingredient_name']]['measure'] = ingredient['measure']
+                ingredients_count[ingredient['ingredient_name']]['quantity'] = int(ingredient['quantity'])*person_count
+    return ingredients_count
+
+
+print(get_shop_list_by_dishes(cook_book.keys(), 3))
